ecc4ba4b018f50c063b73e923febe428c718c24c
Svetlanushka1 <durovasvetlanushka@gmail.com>
2023-11-27 12:37:44 +0200

    AppManager +helpers +userDto without aby tests

---
+++ src/test/java/manager/UserHelper.java
@@ -0,0 +1,40 @@
+package manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import userdto.UserDTO;
+import userdto.UserDTOWith;
+
+public class UserHelper extends BaseHelper{
+
+
+    By btnLoginNavigatorMenu = By.xpath("//a[contains(@href, '/login')]");
+    By inputEmailLoginForm = By.xpath("//input[@id='email']");
+    By inputPasswordLoginForm = By.xpath("//input[@id='password']");
+    By btnYallaLoginForm = By.xpath("//button[@type='submit']");
+    By textSuccessLoginPopUp = By.xpath("//h2[@class='message']");
+
+    public UserHelper(WebDriver driver) {
+        super(driver);
+    }
+
+
+    public void login(UserDTO userDTO) {
+        clickBase(btnLoginNavigatorMenu);
+        typeTextBase(inputEmailLoginForm, userDTO.getEmail());
+        typeTextBase(inputPasswordLoginForm, userDTO.getPassword());
+        clickBase(btnYallaLoginForm);
+    }
+
+    public void login(UserDTOWith userDTO) {
+        clickBase(btnLoginNavigatorMenu);
+        typeTextBase(inputEmailLoginForm, userDTO.getEmail());
+        typeTextBase(inputPasswordLoginForm, userDTO.getPassword());
+        clickBase(btnYallaLoginForm);
+    }
+
+    public boolean validatePopUpMessageSuccessAfterLogin() {
+        return isTextEqual(textSuccessLoginPopUp, "Logged in success");
+    }
+}
+
